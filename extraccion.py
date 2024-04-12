18f0a2ff6145c6e042612f46578cacdbb88d655a
JimmyAguilar03 <Jaime.atrejo03@gmail.com>
2024-04-11 23:21:53 -0600

    actualizacion

--- extraccion.py
+++ extraccion.py
@@ -23,3 +23,32 @@ filtro2.to_csv("Entregable2.csv")
 filtro3=df.iloc[:, [1, 3, 4,10]]  
 print(filtro3)
 filtro3.to_csv("Entregable3.csv")
+
+df1= pd.read_excel('detalle_precios_productos_fabricados_2022.xlsx', index_col=1)
+df1
+
+filtro4=df1.loc[['2022-11-22','2022-12-22'], ['SUBTOTAL_PARTIDA']]  
+print(filtro4)
+filtro4.to_csv("Entregable4.csv")
+
+filtro5=df.head(8)
+print(filtro5)
+filtro5.to_csv("Entregable5.csv")
+
+filtro6=df[df['SUBTOTAL_PARTIDA'] > 77000]
+print(filtro6)
+filtro6.to_csv("Entregable6.csv")
+
+filtro7=df[(df['SUBTOTAL_PARTIDA'] > 77000)& (df['FECHA_DOC']=='2022-05-24')]
+print(filtro7)
+filtro7.to_csv("Entregable7.csv")
+
+filtro8=df[(df['SUBTOTAL_PARTIDA']>77000) | (df['FECHA_DOC']=='2022-03-31')]
+print(filtro8)
+filtro8.to_csv("Entregable8.csv")
+
+filtro9=df[~(df['SUBTOTAL_PARTIDA'] > 77000) & ~(df['FECHA_DOC']=='2022-01-05')] 
+print(filtro9)
+filtro9.to_csv("Entregable9.csv")
+
+
